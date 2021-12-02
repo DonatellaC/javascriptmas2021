294353afcbb30eebe1c35670a7e2f7bc86587362
DonatellaC <donatella.carta@mailbox.org>
2021-12-02 18:08:24 +0100

    added functionality

--- 2-toogle-christmas-theme/index.js
+++ 2-toogle-christmas-theme/index.js
@@ -3,3 +3,20 @@ const greeting = document.getElementById("greeting");
 const christmasBtn = document.getElementById("christmas");
 const snowBtn = document.getElementById("snow");
 const threeBtn = document.getElementById("three");
+
+// Task:
+//- Add the functionality to switch the theme between 'Christmas', 'snow' and 'three'.
+christmasBtn.addEventListener("click", () => {
+  body.className = "christmas";
+  greeting.innerHTML = "🎅 Merry Christmas!";
+});
+
+snowBtn.addEventListener("click", () => {
+  body.className = "snow";
+  greeting.innerHTML = "☃️ Happy Christmas!";
+});
+
+threeBtn.addEventListener("click", () => {
+  body.className = "three";
+  greeting.innerHTML = "🎄 Happy Holidays!";
+});
