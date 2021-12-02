b4e3cfe256139fb61b178115625f250d0e05e3ff
DonatellaC <donatella.carta@mailbox.org>
2021-12-02 18:02:18 +0100

    added three theme

---
+++ 2-toogle-christmas-theme/index.js
@@ -0,0 +1,5 @@
+const body = document.getElementById("body");
+const greeting = document.getElementById("greeting");
+const christmasBtn = document.getElementById("christmas");
+const snowBtn = document.getElementById("snow");
+const threeBtn = document.getElementById("three");
